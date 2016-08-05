e8ff8974af343ce57ea8520f4712ca85518fe7c2
Ilmari Autio <ilmari.autio@gmail.com>
2016-08-05 15:33:30 +0300

    Use Polymer framework

--- renderer-process/client-register/client-register.js
+++ renderer-process/client-register/client-register.js
@@ -3,6 +3,7 @@
   const fs = require('fs');
   const config = remote.require('./config');
   const xlxs = require('xlsx');
+
   const registerFile = config.get('registerFile');
 
   fs.access(registerFile, fs.F_OK, (err) => {
@@ -26,10 +27,23 @@
     const workbook = xlxs.readFile(register);
     const firstSheet = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[firstSheet];
-    return xlxs.utils.sheet_to_json(worksheet);
+    return worksheet;
   }
 
-  function renderRegister(register) {
+  function renderRegister(worksheet) {
+    const range = xlxs.utils.decode_range(worksheet['!ref']);
+    const register = xlxs.utils.sheet_to_json(worksheet);
+    let headers = [];
     
+    const startIdx = range.s.c;
+    const endIdx = range.e.c;
+    const headerRow = range.s.r;
+    //Loop first row and add cell values as table headers
+    for (let i = startIdx; i < endIdx; i++) {
+      const cell = worksheet[{c: i, r: headerRow}];
+      headers.push(cell.v);
+    }
+
+    const frag = document.createDocumentFragment();
   }
 })();
---
+++ renderer-process/client-register/data-grid.html
@@ -0,0 +1,14 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
