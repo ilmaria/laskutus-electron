11c5b2a6ef112af73b5ec5a1d7e70cb637cfcfb0
Ilmari Autio <ilmari.autio@gmail.com>
2016-10-21 21:40:43 +0300

    Add total price line to invoice

--- src/invoice.js
+++ src/invoice.js
@@ -1,7 +1,7 @@
-const PDFDocument = require('pdfkit');
-const fs = require('fs');
-const path = require('path');
-const accounting = require('accounting');
+const PDFDocument = require('pdfkit')
+const fs = require('fs')
+const path = require('path')
+const accounting = require('accounting')
 
 const fieldNames = [
   'nimi',
@@ -14,13 +14,13 @@ const fieldNames = [
   'maksuehto',
   'viivästyskorko',
   'numero'
-];
+]
 
 module.exports = {
   createPdf,
   savePdf,
   fieldNames
-};
+}
 
 function createPdf(clientInfo, productList = [{
   name: 'Käyttövastike',
@@ -35,28 +35,28 @@ function createPdf(clientInfo, productList = [{
   count: 1,
   tax: 0.12
 }]) {
-  const doc = new PDFDocument();
-  const MARGIN_TOP = 40;
-  const MARGIN_LEFT = 40;
-  const MARGIN_RIGHT = 612 - 72;
+  const doc = new PDFDocument()
+  const MARGIN_TOP = 40
+  const MARGIN_LEFT = 40
+  const MARGIN_RIGHT = 612 - 72
   
   const {
     nimi, lähiosoite, postitoimipaikka,
     päiväys, laskunumero, eräpäivä,
     puhelin, maksuehto, viivästyskorko,
     numero
-  } = clientInfo;
+  } = clientInfo
 
   // Company title
-  doc.text('LAPPAJÄRVEN LOMA-GOLF OY', MARGIN_LEFT, MARGIN_TOP);
-  doc.moveDown(3);
+  doc.text('LAPPAJÄRVEN LOMA-GOLF OY', MARGIN_LEFT, MARGIN_TOP)
+  doc.moveDown(3)
   
   // Recipient name and address
   doc.text([
     nimi,
     lähiosoite,
     postitoimipaikka
-  ].join('\n'));
+  ].join('\n'))
 
   // Invoice small details
   doc.text([
@@ -69,7 +69,7 @@ function createPdf(clientInfo, productList = [{
     'Viivästyskorko:',
     '',
     'HUOM! Uusi tilinumero.'
-  ].join('\n'), 315, MARGIN_TOP);
+  ].join('\n'), 315, MARGIN_TOP)
   
   doc.text([
     päiväys,
@@ -81,52 +81,72 @@ function createPdf(clientInfo, productList = [{
     viivästyskorko
   ].join('\n'), 315, MARGIN_TOP, {
     align: 'right'
-  });
+  })
 
   // Product list
-  const PRODUCT_LIST_HEADER = 250;
-  const PRICE = 250;
-  const COUNT = 300;
-  const TAXLESS = 370;
-  const TAX = 430;
-  const TOTAL = 480; 
-  doc.text('Tuote', MARGIN_LEFT, PRODUCT_LIST_HEADER);
-  doc.text('à-hinta', PRICE, PRODUCT_LIST_HEADER);
-  doc.text('Määrä', COUNT, PRODUCT_LIST_HEADER);
-  doc.text('Veroton', TAXLESS, PRODUCT_LIST_HEADER);
-  doc.text('Alv %', TAX, PRODUCT_LIST_HEADER);
+  const PRODUCT_LIST_HEADER = 250
+  const PRICE_H = 250
+  const PRICE = PRICE_H - 10
+  const COUNT_H = 300
+  const COUNT = COUNT_H + 10
+  const TAXLESS_H = 370
+  const TAXLESS = TAXLESS_H - 5
+  const TAX = 430
+  const TOTAL = 480 
+  doc.text('Tuote', MARGIN_LEFT, PRODUCT_LIST_HEADER)
+  doc.text('à-hinta', PRICE_H, PRODUCT_LIST_HEADER)
+  doc.text('Määrä', COUNT_H, PRODUCT_LIST_HEADER)
+  doc.text('Veroton', TAXLESS_H, PRODUCT_LIST_HEADER)
+  doc.text('Alv %', TAX, PRODUCT_LIST_HEADER)
   doc.text('Yhteensä', TOTAL, PRODUCT_LIST_HEADER, {
     align: 'right'
-  });
+  })
 
-  const HEADER_DASH = PRODUCT_LIST_HEADER + 15;
-  doc.lineWidth(1);
+  const HEADER_DASH = PRODUCT_LIST_HEADER + 15
+  doc.lineWidth(1)
   doc.lineCap('butt')
     .moveTo(MARGIN_LEFT - 10, HEADER_DASH)
     .lineTo(MARGIN_RIGHT + 10, HEADER_DASH)
-    .stroke();
+    .stroke()
     
   // Products
-  const START_Y_COORD = HEADER_DASH + 7;
-  let i = 0;
-  for (product of productList) {
-    const yCoord = START_Y_COORD + i*25;
-    doc.text(`${product.name}, ${product.id}`, MARGIN_LEFT, yCoord);
-    doc.text(formatMoney(product.price), PRICE - 10, yCoord);
-    doc.text(product.count, COUNT + 10, yCoord);
-    doc.text(formatMoney(product.price), TAXLESS - 5, yCoord);
-    doc.text(`${product.tax * 100}%`, TAX, yCoord);
-    doc.text(formatMoney(product.price * (1 + product.tax)), TOTAL, yCoord, {
-      align: 'right'
-    });
+  const START_Y_COORD = HEADER_DASH + 7
+  let productYCoord
+  for (let i = 0; i < productList.length; i++) {
+    const product = productList[i]
+    const totalPrice = product.price * product.count
+    productYCoord = START_Y_COORD + i*25
     
-    i++;
+    //name
+    doc.text(`${product.name}, ${product.id}`, MARGIN_LEFT, productYCoord)
+    //à-price
+    doc.text(formatMoney(product.price), PRICE, productYCoord)
+    //count
+    doc.text(product.count, COUNT_H, productYCoord)
+    //total price without tax
+    doc.text(formatMoney(totalPrice), TAXLESS, productYCoord)
+    //tax
+    doc.text(`${product.tax * 100}%`, TAX, productYCoord)
+    //total price
+    doc.text(formatMoney(totalPrice * (1 + product.tax)), TOTAL,
+      productYCoord, {align: 'right'})
   }
   
+  const [totalPrice, totalTax] = productList.reduce((total, product) => {
+    const price = product.price * product.count
+    const tax = price * product.tax
+    return [total[0] + price, total[1] + tax]
+  }, [0, 0])
   
-  doc.end();
+  const TOTAL_Y = productYCoord + 50
+  doc.text('Yhteensä:', PRICE, TOTAL_Y)
+  doc.text(formatMoney(totalPrice), TAXLESS, TOTAL_Y)
+  doc.text(formatMoney(totalPrice + totalTax),
+    TOTAL, TOTAL_Y, {align: 'right'})
+  
+  doc.end()
 
-  return doc;
+  return doc
 }
 
 function formatMoney(number) {
@@ -141,15 +161,15 @@ function formatMoney(number) {
 
 function savePdf(invoiceData, dir) {
   try {
-    fs.accessSync(dir, fs.F_OK);
+    fs.accessSync(dir, fs.F_OK)
   } catch (e) {
-    fs.mkdir(dir);
+    fs.mkdir(dir)
   }
   
-  const name = invoiceData.nimi.replace(/ /g, '_') || 'nimetön';
-  const number = invoiceData.numero;
-  const file = path.join(dir, `${name}_${number}.pdf`);
+  const name = invoiceData.nimi.replace(/ /g, '_') || 'nimetön'
+  const number = invoiceData.numero
+  const file = path.join(dir, `${name}_${number}.pdf`)
 
   createPdf(invoiceData)
-    .pipe(fs.createWriteStream(file));
+    .pipe(fs.createWriteStream(file))
 }
