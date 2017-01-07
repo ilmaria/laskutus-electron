f740a8fd4e81c7be26efde6cebec2373d73e5cfe
Ilmari Autio <ilmari.autio@gmail.com>
2017-01-07 22:10:41 +0200

    Add input validation for product form

--- src/ui/product-editor.ts
+++ src/ui/product-editor.ts
@@ -7,5 +7,30 @@ Polymer({
 
   ready() {
 
+  },
+
+  isValidProduct() {
+    const isValidName = this.$$('[label="Nimi"]').validate()
+    const isValidId = this.$$('[label="Tunnus"]').validate()
+
+    return isValidName && isValidId
+  },
+
+  createProduct() {
+    const name = this.$$('[label="Nimi"]')
+    const id = this.$$('[label="Tunnus"]')
+    const price = this.$$('[label="Hinta"]')
+    const tax = this.$$('[label="Alv"]')
+
+    if (!name.validate() || !id.validate()) {
+      return false
+    }
+
+    return db.put({
+      name: name.value,
+      _id: id.value,
+      price: price.value,
+      tax: tax.value
+    })
   }
 })
